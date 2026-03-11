945d3556d5ab5a54be6a1d748c6010ff17a98d4c
Laure <laureg622@gmail.com>
2026-03-11 11:12:16 +0100

    horizontal scroll done on the user page

--- jellyfishFront/src/App.jsx
+++ jellyfishFront/src/App.jsx
@@ -1,5 +1,5 @@
 import SignIn from './components/signin.jsx';
-import User from './components/user.jsx';
+import User from './components/user/user.jsx';
 import './App.css';
 export default function App() {
   return (

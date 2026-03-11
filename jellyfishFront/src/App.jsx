78436f74d9fd20f304bed6c06fb3c7c4a8f6dca9
Gonçalo Pinto <79314690+Goncalopint@users.noreply.github.com>
2026-03-11 10:57:47 +0000

    Modification for commit

--- jellyfishFront/src/App.jsx
+++ jellyfishFront/src/App.jsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './Home'
+import Collection from './Collection'
+import ItemDetail from './ItemDetail'
 
 function App() {
-  return (
-    <div className="w-full">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
-  )
+    return (
+        <div className="w-full">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Collection" element={<Collection />} />
+                <Route path="/item/:id" element={<ItemDetail />} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App

2fd86eefe34ef20e90c782be74652afab8f46754
Mateo BAL <mateo.bal345@gmail.com>
2026-03-12 22:03:52 +0100

    padding login create account, random collection in home

--- jellyfishFront/src/pages/Create-account.jsx
+++ jellyfishFront/src/pages/Create-account.jsx
@@ -62,7 +62,7 @@ function Register() {
                     <h1 className="text-5xl font-bold text-center">Abysses</h1>
                 </div>
 
-                <div className="w-full flex-1 flex flex-col">
+                <div className="w-full px-6 sm:px-10 flex-1 flex flex-col">
                     <form onSubmit={handleSubmit} className="flex-1 flex flex-col gap-4">
                         <div
                             className="relative w-full h-16 flex flex-row items-center gap-4 p-4 rounded-full border border-[#0081FD] overflow-hidden">
